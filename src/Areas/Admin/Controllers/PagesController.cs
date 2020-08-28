ae8843e1c0c542813e3d58b28312eafe8f62c5bf
fosol <jeremymfoster@hotmail.com>
2020-08-28 15:49:11 -0700

    Adding pages

---
+++ src/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Vic.Api.Areas.Admin.Models.Pages;
+using Vic.Api.Models;
+using Vic.Data;
+using Vic.Data.Entities;
+
+namespace Vic.Api.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// PagesController class, provides a controller for page endpoints.
+    /// </summary>
+    [Authorize]
+    [ApiController]
+    [Area("admin")]
+    [Route("[area]/[controller]")]
+    public class PagesController : ControllerBase
+    {
+        #region Variables
+        private readonly VicContext _context;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of a PagesController object, initializes with specified arguments.
+        /// </summary>
+        /// <param name="context"></param>
+        public PagesController(VicContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Endpoints
+        /// <summary>
+        /// Get a page of pages from the datasource.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="quantity"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get(int page = 1, int quantity = 0)
+        {
+            if (page < 1) page = 1;
+            if (quantity < 0) quantity = 0;
+
+            var query = _context.Pages.OrderBy(p => p.SortOrder).ThenBy(p => p.Name).ThenBy(p => p.Path).Select(p => new PageModel(p));
+
+            if (page > 1 && quantity > 0)
+                query = query.Skip((page - 1) & quantity).Take(quantity);
+
+            var pages = query.ToArray();
+
+            return new JsonResult(new PageModel<PageModel>(page, pages.Count(), pages));
+        }
+
+        /// <summary>
+        /// Fetch the page from the datasource for the specified 'id'.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ResponseCache(Duration = 3600)]
+        public IActionResult Get(int id)
+        {
+            var page = _context.Pages.FirstOrDefault(i => i.Id == id) ?? throw new ArgumentOutOfRangeException("Page does not exist");
+
+            return new JsonResult(new PageModel(page));
+        }
+
+        /// <summary>
+        /// Fetch the page from the datasource for the specified 'path'.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        [HttpGet("path")]
+        [ResponseCache(Duration = 3600)]
+        public IActionResult Get(string path)
+        {
+            var page = _context.Pages.FirstOrDefault(i => i.Path == path) ?? throw new ArgumentOutOfRangeException("Page does not exist");
+
+            return new JsonResult(new PageModel(page));
+        }
+
+        /// <summary>
+        /// Add the new page to the datasource.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public IActionResult Add(PageModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            var page = new Page()
+            {
+                Name = model.Name,
+                Path = model.Path,
+                SortOrder = model.SortOrder,
+                Body = model.Body,
+                IsEnabled = model.IsEnabled
+            };
+
+            _context.Pages.Add(page);
+            _context.SaveChanges();
+
+            return new JsonResult(new PageModel(page));
+        }
+
+        /// <summary>
+        /// Update the page for the specified 'id'.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, PageModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            var page = _context.Pages.FirstOrDefault(i => i.Id == id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Page does not exist.");
+
+            page.Name = model.Name;
+            page.Path = model.Path;
+            page.SortOrder = model.SortOrder;
+            page.Body = model.Body;
+            page.IsEnabled = model.IsEnabled;
+
+            _context.Update(page);
+            _context.SaveChanges();
+
+            return new JsonResult(new PageModel(page));
+        }
+
+        /// <summary>
+        /// Fetch the page from the datasource for the specified 'id'.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var page = _context.Pages.FirstOrDefault(i => i.Id == id) ?? throw new ArgumentOutOfRangeException("Page does not exist");
+            _context.Pages.Remove(page);
+            _context.SaveChanges();
+
+            return new JsonResult(new PageModel(page));
+        }
+        #endregion
+    }
+}
---
+++ src/Areas/Admin/Models/Pages/PageModel.cs
@@ -0,0 +1,35 @@
+﻿using Vic.Data.Entities;
+
+namespace Vic.Api.Areas.Admin.Models.Pages
+{
+    public class PageModel
+    {
+        #region Properties
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public string Body { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public bool IsEnabled { get; set; }
+        #endregion
+
+        #region Constructors
+        public PageModel() { }
+
+        public PageModel(Page page)
+        {
+            this.Id = page.Id;
+            this.Name = page.Name;
+            this.Path = page.Path;
+            this.Body = page.Body;
+            this.SortOrder = page.SortOrder;
+            this.IsEnabled = page.IsEnabled;
+        }
+        #endregion
+    }
+}
